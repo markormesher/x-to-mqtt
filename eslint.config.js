9374aa6b58497df0f52bde6f786f8b4162df8b3d
Mark Ormesher <me@markormesher.co.uk>
2023-10-31 11:03:55 +0000

    remove unused eslint override

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,3 @@
 import config from "@markormesher/eslint-config";
 
-export default [
-  ...config,
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      // this project has a lot of inherited methods or callbacks with an async signature that don't always need to be async
-      "@typescript-eslint/require-await": "off",
-    },
-  },
-];
+export default config;
